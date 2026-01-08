8e0d344bf83f6d5cb1d2ba6aa8068e6701dabffc
Mazuru Gabriel <gabrielmazuru6@gmail.com>
2026-01-08 17:53:39 +0200

    added one more button and led

--- piano.ino
+++ piano.ino
@@ -1,11 +1,11 @@
 int buzzerPin = A5;
-int Buttons[] = {2, 3, 4, 5};
-int Leds[] = {6, 7, 8, 9};
-int tones[] = {440, 494, 523, 587};
+int Buttons[] = {2, 3, 4, 5,12};
+int Leds[] = {6, 7, 8, 9, 10};
+int tones[] = {440, 494, 523, 587, 659};
 
 void setup() {
   pinMode(buzzerPin, OUTPUT);
-  for (int i = 0; i < 4; i++) {
+  for (int i = 0; i < 5; i++) {
     pinMode(Buttons[i], INPUT_PULLUP);
     pinMode(Leds[i], OUTPUT);
   }
@@ -14,7 +14,7 @@ void setup() {
 void loop() {
   bool active = false;
 
-  for (int i = 0; i < 4; i++) {
+  for (int i = 0; i < 5; i++) {
     if (digitalRead(Buttons[i]) == LOW) {
       tone(buzzerPin, tones[i]);
       digitalWrite(Leds[i], HIGH);
